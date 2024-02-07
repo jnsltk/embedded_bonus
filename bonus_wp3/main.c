3a71a8ac6f0be7e2855b7a34f1edc41a44e924ff
jnsltk <janos.litkei@gmail.com>
2024-02-07 23:56:07 +0100

    int input with strtok

--- bonus_wp3/main.c
+++ bonus_wp3/main.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
-int matrix1[4][4] = {{1, 2}, {4, 5}};
-int matrix2[4][4] = {{9, 8}, {6, 5}};
+#include <stdlib.h>
+#include <string.h>
 
-unsigned char array_x = 2;
-unsigned char array_y = 2;
-
-void get_input(unsigned char matrix_size, int matrix[4][4]);
+void input_matrix(int ***matrix, int *rows, int *cols);
+int get_size(int *rows, int *cols);
+int *get_int_input(int size);
 
 int main(int argc, char **argv) {
-    char c;
-    char i = 0;
-    int n = 0;
-    while ((c = getchar()) != EOF) {
-        if (c == ' ') {
-            printf("%d ", n);
-            n = 0;
-            i++;
-            continue;
-        } else if (c == '\n') {
-            printf("%d\n", n);
-            i++;
-            break;
-        } else if (n != 0) {
-            n *= 10;
-        }
-        n += c - 48;
+    int rows;
+    int cols;
+
+    int *m_size = get_int_input(2);
+
+    if (m_size == NULL) {
+        return 2;
     }
-    printf("%d\n", i);
+
+    rows = m_size[0];
+    cols = m_size[1];
+
+    free(m_size);
+
+    int *a = get_int_input(cols);
+
+    if (a == NULL) {
+        return 2;
+    }
+
+    for (char i = 0; i < cols; i++) {
+        printf("%d ", a[i]);
+    }
+
+    free(a);
+    return 0;
+
+    int **matrix1 = (int **) malloc(sizeof(int *) * cols);
+    for (char i = 0; i < 4; i++) {
+        matrix1[i] = (int *) malloc(sizeof(int *) * rows);
+    }
+
+    int **matrix2 = (int **) malloc(sizeof(int *) * cols);
+    for (char i = 0; i < 4; i++) {
+        matrix2[i] = (int *) malloc(sizeof(int *) * rows);
+    }
+
     // int result[array_y][array_x];
     // for (char i = 0; i < array_y; i++) {
     //     for (char j = 0; j < array_x; j++) {
@@ -40,4 +57,37 @@ int main(int argc, char **argv) {
     //     }
     //     putchar('\n');
     // }
+}
+
+void input_matrix(int ***matrix, int *rows, int *cols) {}
+
+int *get_int_input(int size) {
+    char a[100], *result;
+    int *b = malloc(sizeof(int) * size);
+
+    if ((result = fgets(a, sizeof(a), stdin)) == NULL)
+        printf("Invalid input!\n");
+
+    char *p = strtok(a, " ");
+
+    char i = 0;
+    while (p != NULL) {
+        char *end;
+        b[i++] = strtol(p, &end, 10);
+        if (p == end) {
+            printf("Invalid input\n");
+            return NULL;
+        }
+        if (i > size) {
+            printf("Wrong number of digits! Please enter exactly %d!\n", size);
+            return NULL;
+        }
+        p = strtok(NULL, " ");
+    }
+    if (i < size) {
+        printf("Wrong number of digits! Please enter exactly %d!\n", size);
+        return NULL;
+    }
+
+    return b;
 }

64b5036b3d3a50586466ba0c21b7e98c857558d2
jnsltk <janos.litkei@gmail.com>
2024-02-08 12:59:50 +0100

    fix everything for codegrade

--- bonus_wp3/main.c
+++ bonus_wp3/main.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void input_matrix(int ***matrix, int *rows, int *cols);
-int get_size(int *rows, int *cols);
 int *get_int_input(int size);
+int ***init_matrix(int rows, int cols, int *a);
+int ***add_matrices(int ***matrix1, int ***matrix2, int rows, int cols, int *a);
+void print_matrix(int ***matrix, int rows, int cols);
+void free_matrix(int ***matrix, int rows);
 
 int main(int argc, char **argv) {
     int rows;
     int cols;
 
+    printf("Input the size: ");
     int *m_size = get_int_input(2);
 
     if (m_size == NULL) {
-        return 2;
+        return 0;
     }
 
     rows = m_size[0];
@@ -21,45 +24,44 @@ int main(int argc, char **argv) {
 
     free(m_size);
 
-    int *a = get_int_input(cols);
+    if (rows == 1 && cols == 1) {
+        printf("Invalid matrix!");
+        return 0;
+    }
+
+    printf("Input elements of matrix 1: ");
+    int *a = get_int_input(cols * rows);
 
     if (a == NULL) {
-        return 2;
+        return 0;
     }
 
-    for (char i = 0; i < cols; i++) {
-        printf("%d ", a[i]);
+    printf("Input elements of matrix 2: ");
+    int *b = get_int_input(cols * rows);
+
+    if (b == NULL) {
+        return 0;
     }
+
+    int ***matrix1 = init_matrix(rows, cols, a);
+    int ***matrix2 = init_matrix(rows, cols, b);
+
+    int *c = malloc(sizeof(int) * rows * cols);
+
+    int ***res_matrix = add_matrices(matrix1, matrix2, rows, cols, c);
+
+    printf("\nThe sum is: \n");
+    print_matrix(res_matrix, rows, cols);
+
+    free_matrix(matrix1, rows);
+    free_matrix(matrix2, rows);
+    free_matrix(res_matrix, rows);
 
     free(a);
+    free(b);
+    free(c);
     return 0;
-
-    int **matrix1 = (int **) malloc(sizeof(int *) * cols);
-    for (char i = 0; i < 4; i++) {
-        matrix1[i] = (int *) malloc(sizeof(int *) * rows);
-    }
-
-    int **matrix2 = (int **) malloc(sizeof(int *) * cols);
-    for (char i = 0; i < 4; i++) {
-        matrix2[i] = (int *) malloc(sizeof(int *) * rows);
-    }
-
-    // int result[array_y][array_x];
-    // for (char i = 0; i < array_y; i++) {
-    //     for (char j = 0; j < array_x; j++) {
-    //         result[i][j] = matrix1[i][j] + matrix2[i][j];
-    //     }
-    // }
-
-    // for (char i = 0; i < array_y; i++) {
-    //     for (char j = 0; j < array_x; j++) {
-    //         printf("%d ", result[i][j]);
-    //     }
-    //     putchar('\n');
-    // }
 }
-
-void input_matrix(int ***matrix, int *rows, int *cols) {}
 
 int *get_int_input(int size) {
     char a[100], *result;
@@ -68,7 +70,9 @@ int *get_int_input(int size) {
     if ((result = fgets(a, sizeof(a), stdin)) == NULL)
         printf("Invalid input!\n");
 
-    char *p = strtok(a, " ");
+    char *s = " \n";
+
+    char *p = strtok(a, s);
 
     char i = 0;
     while (p != NULL) {
@@ -78,16 +82,62 @@ int *get_int_input(int size) {
             printf("Invalid input\n");
             return NULL;
         }
-        if (i > size) {
-            printf("Wrong number of digits! Please enter exactly %d!\n", size);
+        if (i > size + 1) {
+            printf("Invalid input! Please enter exactly %d digits!\n", size);
             return NULL;
         }
-        p = strtok(NULL, " ");
+        p = strtok(NULL, s);
     }
     if (i < size) {
-        printf("Wrong number of digits! Please enter exactly %d!\n", size);
+        printf("Invalid input! Please enter exactly %d digits!\n", size);
         return NULL;
     }
 
     return b;
+}
+
+int ***init_matrix(int rows, int cols, int *a) {
+    int ***matrix = malloc(sizeof(int **) * rows);
+    for (char i = 0; i < rows; i++)
+        matrix[i] = malloc(sizeof(int **) * cols);
+
+    char k = 0;
+    for (char i = 0; i < rows; i++) {
+        for (char j = 0; j < cols; j++) {
+            matrix[i][j] = &a[k];
+            k++;
+        }
+    }
+    return matrix;
+}
+
+int ***add_matrices(int ***matrix1, int ***matrix2, int rows, int cols, int *a) {
+    int ***res_matrix = malloc(sizeof(int **) * rows);
+    for (char i = 0; i < rows; i++)
+        res_matrix[i] = malloc(sizeof(int **) * cols);
+
+    char k = 0;
+    for (char i = 0; i < rows; i++) {
+        for (char j = 0; j < cols; j++) {
+            a[k] = *matrix1[i][j] + *matrix2[i][j];
+            res_matrix[i][j] = &a[k];
+            k++;
+        }
+    }
+    return res_matrix;
+}
+
+void print_matrix(int ***matrix, int rows, int cols) {
+    for (char i = 0; i < rows; i++) {
+        for (char j = 0; j < cols; j++) {
+            printf("%d ", *matrix[i][j]);
+        }
+        putchar('\n');
+    }
+}
+
+void free_matrix(int ***matrix, int rows) {
+    for (char i = 0; i < rows; i++)
+        free(matrix[i]);
+    free(matrix);
 }
